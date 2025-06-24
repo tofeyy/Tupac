2f6113db700026bc15b82f437522c32b6be59a07
Tofey <105518597+tofeyy@users.noreply.github.com>
2025-06-25 01:59:42 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// تأكد هذا السطر يحتوي على اسم المشروع داخل GitHub
+export default defineConfig({
+  plugins: [react()],
+  base: '/Tupac/', // <-- هذا اسم المستودع على GitHub
+})
